34026b694b8f9e0c5253dfcec8b09dd7c1823473
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2022-06-15 11:13:24 -0300

    [main] add telegram functionality

--- Configs.py
+++ Configs.py
@@ -11,12 +11,12 @@ class Configs:
     lastnotifid = -1
 
     @property
-    def roomkey(self):
-        return self.cfg['ntfy.key']
+    def key(self):
+        return self.cfg[self.method + '.key']
 
-    @roomkey.setter
-    def roomkey(self, value):
-        self.cfg['ntfy.key'] = value
+    @key.setter
+    def key(self, value):
+        self.cfg[self.method + '.key'] = value
         self.update()
 
     @property
@@ -28,11 +28,29 @@ class Configs:
         self.cfg['all.method'] = value
         self.update()
 
+    @property
+    def interval(self):
+        return self.cfg['all.interval']
+
+    @interval.setter
+    def interval(self, value):
+        self.cfg['all.interval'] = value
+        self.update()
+
+    @property
+    def chatid(self):
+        return self.cfg[methods[1] + '.chatid']
+
+    @chatid.setter
+    def chatid(self, value):
+        self.cfg[methods[1] + '.chatid'] = value
+        self.update()
+
     def __init__(self, cfgpath='cfg.ini'):
         self.cfgpath = cfgpath
         file = open('cfg.ini', 'a+')
         if os.stat(cfgpath).st_size < 15:
-            self.cfg = benedict({'all': {'method': methods[0]}, methods[0]: {'key': ''}, methods[1]: {}})
+            self.cfg = benedict({'all': {'method': methods[0], 'interval': 5}, methods[0]: {'key': ''}, methods[1]: {'key': '', 'chatid': ''}})
             self.update()
         else:
             self.cfg = benedict.from_ini(self.cfgpath)
--- notif.py
+++ notif.py
@@ -4,6 +4,7 @@ import string
 import threading
 
 import requests
+import telegram
 import PySimpleGUI as sg
 from winsdk.windows.foundation.metadata import ApiInformation
 from winsdk.windows.ui.notifications.management import UserNotificationListener, UserNotificationListenerAccessStatus
@@ -16,7 +17,7 @@ listening = False
 
 methodsDesc = {
     methods[0]: 'Input your ntfy theme name below:',
-    methods[1]: 'Input your telegram bot theme name below:'
+    methods[1]: 'Input your telegram bot apikey and desired chat id below:'
 }
 methodsFn = {
     methods[0]: lambda title, text: sendNtfy(title, text),
@@ -26,13 +27,16 @@ methodsFn = {
 stopping = False
 stopApp = False
 
+bot = None
+
 def sendTelegram(title, text):
-    pass
+    print('Sending notification with title ' + title + ' to telegram.')
+    bot.send_message(chat_id=cfg.chatid, text='<i><b>' + title + '</b></i>\n\n' + text, parse_mode='html')
 
 
 def sendNtfy(title, text):
-    print('Sending ntfy with title ' + title + ' to room ' + cfg.roomkey + '.')
-    requests.post("https://ntfy.sh/" + cfg.roomkey,
+    print('Sending ntfy with title ' + title + ' to room ' + cfg.key + '.')
+    requests.post("https://ntfy.sh/" + cfg.key,
                   data=text.encode(encoding='utf-8'),
                   headers={"Title": title.encode('utf-8')})
 
@@ -74,7 +78,7 @@ async def startListening():
         print('Found ' + str(len(list)) + ' new notifs, sending...')
         onNotification(list)
         try:
-            await asyncio.sleep(10)
+            await asyncio.sleep(cfg.interval)
         except asyncio.CancelledError:
             onStopped()
             break
@@ -99,9 +103,7 @@ def treatNotifTextElements(notif):
 
 def onNotification(toasts):
     for toast in toasts:
-        treatNotifTextElements(
-            toast.notification.visual.get_binding(KnownNotificationBindings.get_toast_generic()).get_text_elements())
-
+        treatNotifTextElements(toast.notification.visual.get_binding(KnownNotificationBindings.get_toast_generic()).get_text_elements())
 
 def onStopped():
     global window, stopping
@@ -115,10 +117,22 @@ def onStopped():
     window['startbtn'].update('Start listening', disabled=False)
     window['secretkey'].update(disabled=False)
     window['changedmethod'].update(disabled=False)
+    window['intervalcombo'].update(disabled=False)
+    window['chatid'].update(disabled=False)
 
 
 def id_generator(size=20, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
+
+
+def change_method():
+    global window
+    window['startbtn'].update('Start listening')
+    stopListening()
+    window['secretkey'].update(cfg.key)
+    window['methoddesc'].update(methodsDesc[cfg.method])
+    window['chatid'].update(visible=cfg.method == methods[1])
+    window['generaterandombtn'].update(visible=cfg.method == methods[0])
 
 
 loop = asyncio.new_event_loop()
@@ -131,16 +145,21 @@ layout = [
         sg.Text("Select your desired notification method:"),
     ],
     [
-        sg.Combo([methods[0]], default_value=cfg.method, readonly=True, size=(15, 1), enable_events=True,
+        sg.Combo(methods, default_value=cfg.method, readonly=True, size=(15, 1), enable_events=True,
                  key='changedmethod'),
         sg.Text("Learn more...", enable_events=True, font=('Arial', 8, 'underline'))
     ],
     [
-        sg.Text("Input your ntfy theme name below:", key='methoddesc'),
+        sg.Text(methodsDesc[cfg.method], key='methoddesc'),
     ],
     [
-        sg.In(size=(30, 1), key='secretkey', default_text=cfg.roomkey),
-        sg.Button("Generate random", key="generaterandombtn")
+        sg.In(size=(30, 1), key='secretkey', default_text=cfg.key),
+        sg.Button("Generate random", key="generaterandombtn", visible=cfg.method == methods[0]),
+        sg.In(size=(15, 1), default_text=cfg.chatid, key="chatid", visible=cfg.method == methods[1])
+    ],
+    [
+        sg.Text("Refresh interval:"),
+        sg.Combo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 60], default_value=cfg.interval, size=(15, 1), enable_events=True, key='intervalcombo')
     ],
     [
         sg.Button(button_text="Start listening", key="startbtn", pad=(0, 10))]
@@ -153,26 +172,37 @@ window = sg.Window(title="Win10NotificationSynchronizer",
 while True:
     event, values = window.read()
 
+    if event == 'intervalcombo':
+        cfg.interval = values['intervalcombo']
+
     if event == 'generaterandombtn':
         key = id_generator()
         window['secretkey'].update(key)
-        cfg.roomkey = key
+        cfg.key = key
 
     if event == 'changedmethod':
         cfg.method = values['changedmethod']
-        stopListening()
-        window['startbtn'].update('Start listening')
-        window['secretkey'].update('')
-        window['methoddesc'].update(methodsDesc[cfg.method])
+        change_method()
 
     if event == 'startbtn' and not stopping:
-        cfg.roomkey = values['secretkey']
+        cfg.key = values['secretkey']
         listening = not listening
         if listening:
+            if cfg.method == methods[1]:
+                cfg.chatid = values['chatid']
+                try:
+                    bot = telegram.Bot(token=cfg.key)
+                except telegram.error.InvalidToken:
+                    sg.Popup('Invalid api key.', keep_on_top=True)
+                    listening = False
+                    continue
+
             window['startbtn'].update('Stop listening')
             task = loop.create_task(startListening())
             window['secretkey'].update(disabled=True)
             window['changedmethod'].update(disabled=True)
+            window['intervalcombo'].update(disabled=True)
+            window['chatid'].update(disabled=True)
 
             try:
                 threading.Thread(target=lambda: loop.run_until_complete(task)).start()
